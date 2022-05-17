a431cad9112672578048ec5ef2151b9d90667f1e
SiemenSlabbinck <siemen.slabbinck@student.ap.be>
2022-05-17 09:28:02 +0200

    Add camera and gun rotation

--- VR_Project_BombGunner/Assets/Scripts/Gun.cs
+++ VR_Project_BombGunner/Assets/Scripts/Gun.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public Bullet bulletPrefab;
-    // Update is called once per frame
+    private GameObject gun;
+    void Start() {
+      gun = GameObject.FindWithTag("Gun");
+    }
+
     void Update()
     {
       if (Input.GetKeyDown("space")){
             Bullet bullet = Instantiate(bulletPrefab);
-            bullet.transform.parent = GameObject.Find("Gun").transform;
-            bullet.transform.localPosition = new Vector3(0f, 0f, 0f);
+            bullet.transform.parent = GameObject.Find("Interactive").transform;
+            bullet.transform.localPosition = gun.transform.position;
+            bullet.transform.localRotation = gun.transform.rotation;
             bullet.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
             Debug.Log(GameObject.Find("Gun").transform);
       }  
